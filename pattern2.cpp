8298604dfebca95217128574f6596428ee405af9
Manoj Joshi <108046894+Mj1781@users.noreply.github.com>
2023-02-21 15:34:53 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -3,14 +3,8 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	int i=1;
-	while(i<=n){
-		int j=1;
-		while(j<=n){
-			cout<<i;
-			j=j+1;
-	}
-	cout<<endl;
-		i=i+1;
-}
+	for(int i=1;i<=n;i++){
+		for(int j=i;j<=n;j--){
+			cout<<"*"<<endl;
+		}	}
 }
